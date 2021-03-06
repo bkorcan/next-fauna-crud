9c74b6f48590bcdc8352be1327db166870952f62
bkorcan <bkorcan@gmail.com>
2021-03-06 18:05:19 -0500

    v02

--- pages/customers/[id]/index.js
+++ pages/customers/[id]/index.js
@@ -2,10 +2,32 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import useSWR from 'swr';
 import Layout from '../../../components/layout';
+// 
+import React from 'react';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
+// 
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 const Customer = () => {
+  // 
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true); 
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
+// 
   const router = useRouter();
   const { id } = router.query;
 
@@ -50,7 +72,11 @@ const Customer = () => {
           </div>
         </div>
       ) : (
-        <div>loading...</div>
+        <div>
+          <Backdrop className={classes.backdrop} open={open} >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+        </div>
       )}
 
       <style jsx>{`
