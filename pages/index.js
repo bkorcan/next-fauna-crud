73958efaa53bfcacdf6dd2bfbd2705e3f2ed442f
bkorcan <bkorcan@gmail.com>
2021-03-06 16:52:24 -0500

    v01

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,96 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import useSWR from 'swr';
+import Layout from '../components/layout';
+import DataRow from '../components/data-row';
 
-export default function Home() {
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
+const Home = () => {
+  const { data, error } = useSWR('/api/customers', fetcher);
+
+  if (error) return <div>failed to load</div>;
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <h1>Next Fauna CRUD</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Link href="/customers/create">
+        <a className="createNew">Create New Customer</a>
+      </Link>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="table">
+        <h2>Customer Data</h2>
+        <div className="headerRow">
+          <h4>name</h4>
+          <h4>telephone</h4>
+          <h4 className="creditCard">credit card</h4>
         </div>
-      </main>
+        {data ? (
+          data.map((d) => (
+            <DataRow
+              key={d.ref['@ref'].id}
+              id={d.ref['@ref'].id}
+              firstName={d.data.firstName}
+              lastName={d.data.lastName}
+              telephone={d.data.telephone}
+              creditCard={d.data.creditCard.number}
+            />
+          ))
+        ) : (
+          <>
+            <DataRow loading />
+            <DataRow loading />
+            <DataRow loading />
+          </>
+        )}
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+      <style jsx>{`
+        h2 {
+          font-size: 16px;
+          font-weight: 600;
+          margin: 0;
+          padding: 0 32px;
+        }
+        h4 {
+          color: #555;
+          font-size: 12px;
+          font-weight: 400;
+          text-transform: uppercase;
+        }
+        .createNew {
+          display: inline-block;
+          background-color: #0070f3;
+          border-radius: 3px;
+          color: #fff;
+          padding: 0.5rem 1rem;
+          margin-bottom: 1rem;
+        }
+
+        .createNew:hover {
+          text-decoration: none;
+        }
+
+        .table {
+          border: 1px solid #eaeaea;
+          border-radius: 4px;
+          min-width: 512px;
+          padding-top: 24px;
+        }
+
+        .headerRow {
+          display: grid;
+          grid-auto-flow: column;
+          grid-template-columns: 1fr 1fr 1fr;
+          padding: 0 32px;
+        }
+
+        .creditCard {
+          margin-left: auto;
+        }
+      `}</style>
+    </Layout>
+  );
+};
+
+export default Home;
